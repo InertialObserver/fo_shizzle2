b0c0812c46a80e51a8a8570d0697a0958832817a
inertial.observer <student@epicodus.com>
2015-05-27 11:45:56 -0700

    change the letter s to the letter z except when capitalized or first letter of the word

--- lib/fo_shizzle.rb
+++ lib/fo_shizzle.rb
@@ -3,16 +3,14 @@ class String
 
 
   define_method(:fo_shizzle) do
+    mo_shizzle_array = []
     shizzle_array = self.split
     shizzle_array.each do |word|
-      word.capitalize!()
-      end
-     shizzle_array.join(" ")
-
-    # pre_shizzle.gsub(/s/, 'z')
-
-
-#     shizzle_phrase = self.sub(/s/, 'z')
-# shizzle_phrase
+      temp_shizzle = word.slice(1, word.length)
+      temp_shizzle_shizzled = temp_shizzle.gsub(/s/, 'z')
+      fo_shizzled = word[0].concat(temp_shizzle_shizzled)
+      mo_shizzle_array.push(fo_shizzled)
+    end
+    mo_shizzle = mo_shizzle_array.join(" ")
   end
 end
--- spec/fo_shizzle2_spec.rb
+++ spec/fo_shizzle2_spec.rb
@@ -4,32 +4,36 @@ require('fo_shizzle')
 describe('fo_shizzle') do
 
   it('returns a string entered by user') do
-    expect(("Super spass").fo_shizzle()).to(eq("Super Spass"))
+    expect(("Super spass").fo_shizzle()).to(eq("Super spazz"))
   end
 
 
   it('splits initial phrase into an array') do
-    expect(("Super spass").fo_shizzle()).to(eq("Super Spass"))
+    expect(("Super spass").fo_shizzle()).to(eq("Super spazz"))
   end
 
   it('changes lowercase s to z in all words') do
-    expect(("Sassy sub suspends").fo_shizzle()).to(eq("Sassy Sub Suspends"))
+    expect(("Sassy sub suspends").fo_shizzle()).to(eq("Sazzy sub suzpendz"))
   end
 
   it('does not change the first letter of any word') do
-    expect(("Sassy sub suspends").fo_shizzle()).to(eq("Sassy Sub Suspends"))
+    expect(("Sassy sub suspends").fo_shizzle()).to(eq("Sazzy sub suzpendz"))
   end
 
   it('changes phrase to title case') do
-    expect(("Sassy sub suspends").fo_shizzle()).to(eq("Sassy Sub Suspends"))
+    expect(("Sassy sub suspends").fo_shizzle()).to(eq("Sazzy sub suzpendz"))
   end
 
   it('joins words from array to string') do
-    expect(("Sassy Sub Suspends").fo_shizzle()).to(eq("Sassy Sub Suspends"))
+    expect(("Sassy Sub Suspends").fo_shizzle()).to(eq("Sazzy Sub Suzpendz"))
   end
 
   it('changes letter s to z') do
-    expect(("Sassy Sub Suspends").fo_shizzle()).to(eq("Sassy Sub Suspends"))
-end
+    expect(("Massy sub suspends").fo_shizzle()).to(eq("Mazzy sub suzpendz"))
+  end
+
+  it('changes letter s to z unless it is the first letter of the word') do
+    expect(("Massy sub suspends").fo_shizzle()).to(eq("Mazzy sub suzpendz"))
+  end
 
 end
